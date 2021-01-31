b33a746570baaf97f6e8272b5d7e0000e095b947
Jorge Turrado <jturrado@plainconcepts.com>
2021-01-31 17:23:51 +0100

    Add project files.

---
+++ ManualApproval/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ManualApproval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var env = Environment.GetEnvironmentVariable("MyVal");
+            if(!string.IsNullOrWhiteSpace(env))
+                Console.WriteLine($"The value of MyVal is:{env}");
+            Console.WriteLine("MyVal is not defined");
+        }
+    }
+}
